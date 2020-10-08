f450edb580e14831dab744b3b9301696c11092a9
Victor Vazquez <yoquesequeponerle@gmail.com>
2020-10-08 13:48:55 -0500

    Creacion de presentacion.c
    Se creo archivo con codigo fuente

---
+++ Presentacion.c
@@ -0,0 +1,12 @@
+#include<stdio.h> //stdio -> Standard Input/Output
+
+int main(){
+	/*My first
+	 * Program
+	 * in C
+	 */
+	printf("Hola Mundo, me llamo: ");
+	printf("Victor Vazquez!");
+	//Comment
+	return 0;
+}
